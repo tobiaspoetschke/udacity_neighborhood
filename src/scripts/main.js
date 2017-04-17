f35a12186f043b6eda84e93d14ad54f436fb63a8
Tobias Pötschke <tobiaspoetschke@googlemail.com>
2017-04-17 13:19:55 +0200

    setup bower and local fallbacks(for unavailable cdn-resources)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,31 @@
 require.config({
     baseUrl: "scripts",
     paths: {
-        "jquery": "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min",
-        "knockout": "https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min",
-        //"gmaps": "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyBpdJY2tYrSbm60O2bZULYlS1IRnHxAgZ4&callback=initMap"
-        "gmaps": "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyBpdJY2tYrSbm60O2bZULYlS1IRnHxAgZ4"
+        "knockout": [
+            "https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min",
+            "../libs/knockout/dist/knockout"
+        ],
+        "gmaps": [
+            "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyBpdJY2tYrSbm60O2bZULYlS1IRnHxAgZ4"
+            //getting the fallback working was to difficult/time-consuming, therefore it will be handled by requirejs.onError
+        ]
     }
 });
 
+/*
+ *  error-handling in the requirejs-context
+ */
+requirejs.onError = function(error) {
+
+    //this should only happen, when the google-maps-api is unavailable
+    if(error.requireType === 'scripterror') {
+        window.alert("Sorry, but the google maps api is unavailable.");
+    } else if(error) {
+        window.alert("Sorry, something went wrong:\n" + error);
+    }
+};
+
+//main-entry-point for the app
 require(["mapComponent", "appViewModel", "knockout"], function(mapComponent, appViewModel, ko){
     mapComponent.initialize();
     ko.applyBindings(new appViewModel());
